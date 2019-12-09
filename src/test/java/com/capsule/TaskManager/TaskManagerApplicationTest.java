6e08bd495929f774d21a8f9fd7e9d405db917b90
Ferozmo <fasi_98212@yahoo.co.in>
2019-12-10 00:02:50 +0400

    retrofit

---
+++ src/test/java/com/capsule/TaskManager/TaskManagerApplicationTest.java
@@ -0,0 +1,14 @@
+package com.capsule.TaskManager;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TaskManagerApplicationTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
